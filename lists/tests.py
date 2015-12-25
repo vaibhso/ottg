80bd3c582022730d1283e6746107577507d20fe2
Vaibhav <vaibhso@gmail.com>
2015-12-25 16:58:30 +0530

    three strikes and refactor

--- lists/tests.py
+++ lists/tests.py
@@ -12,6 +12,14 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+                'home.html',
+                {'new_item_text': 'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
