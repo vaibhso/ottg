a20169920e680a8765e05b930072541b3f3c1591
Vaibhav <vaibhso@gmail.com>
2015-12-26 12:30:55 +0530

    adjusting the resto fo teh world to our new models -

---
+++ lists/migrations/0002_auto_20151226_0655.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(default=None, to='lists.List'),
+        ),
+    ]
